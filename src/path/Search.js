19f6a9b7caa76e65bbe3d51aa365626eda9d73c1
FunkyFrog0813 <saisun656@gmail.com>
2024-09-24 20:28:08 +0800

    remove pagination

--- src/path/Search.js
+++ src/path/Search.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
 import axios from "axios"
 import { Link } from 'react-router-dom'
-import pagination from '../pagination';
 
 const Search=()=>{
     const [brand, setbrands]=useState({
